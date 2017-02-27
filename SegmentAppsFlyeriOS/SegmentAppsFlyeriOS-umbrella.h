9549c73f330048a0880acce90118df9fbebe162a
Kevin Cathaly <alakme@gmail.com>
2017-02-27 13:02:10 +0100

    initial commit

---
+++ SegmentAppsFlyeriOS/SegmentAppsFlyeriOS-umbrella.h
@@ -0,0 +1,17 @@
+//
+//  SegmentAppsFlyeriOS-umbrella.h
+//  SegmentAppsFlyeriOS
+//
+//  Created by Kevin CATHALY on 27/02/17.
+//  Copyright © 2017 Kevin CATHALY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SEGAppsFlyerIntegration.h"
+#import "SEGAppsFlyerIntegrationFactory.h"
+
+FOUNDATION_EXPORT double SegmentAppsFlyeriOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char SegmentAppsFlyeriOSVersionString[];
+
+
